1e84610e9fc29cfb81b4a5e134b5dad7d644d124
Juan Francisco Aguirre <juan.aguirre@naranja.com>
2021-04-27 14:29:02 -0300

    poke pokechu

--- flitroPuchamon/src/scripts/index.js
+++ flitroPuchamon/src/scripts/index.js
@@ -1,50 +1,32 @@
 const pokeUrl = 'https://pokeapi.co/api/v2/pokemon/' // Default URL for API
-const inputName = document.getElementById('inputName')
-const buttonSearch = document.getElementById('searchButton')
-const appNode = document.getElementById('app')
 
 // Define data v
-const pokemonName = document.createElement('h2')
-const pokemonImage = document.createElement('img')
-const pokemonExperience = document.createElement('h2')
-const pokemonHeight = document.createElement('h2')
-const pokemonWeight = document.createElement('h2')
-const container = document.createElement('div')
-const pokemonAbilities = document.createElement('h2')
-const pokemonTypes = document.createElement('h2')
+const pokemonName = document.getElementById('nombrePokemon')
+const pokemonImage = document.getElementById('imgPokemon')
+const pokemonExperience = document.getElementById('xpPokemon')
+const pokemonHeight = document.getElementById('alturaPokemon')
+const pokemonWeight = document.getElementById('pesoPokemon')
+const pokemonAbilities = document.getElementById('habilidadesPokemon')
+const pokemonTypes = document.getElementById('tipoPokemon')
 
-let pokemonAbilitiesTitle = ''
-let pokemonAbility1 = ''
-let pokemonAbility2 = ''
-let pokemonAbility3 = ''
-let pokemonTypeTitle = ''
-let pokemonType1 = ''
-let pokemonType2 = ''
-
-searchButton.addEventListener('click', insertPokemon)
-
-inputName.addEventListener('keydown', (event) => {
-  if (event.key === 'Enter') {
-    searchButton.click()
-  }
-})
-
-function resetPokemon() {
-  let allPokemon = appNode.childNodes
-  allPokemon = Array.from(allPokemon)
-
-  allPokemon.forEach((pokemon) => {
-    pokemon.remove(pokemon)
-  })
+let pokemon = {
+  name: '-',
+  type: '-',
+  weight: '-',
+  height: '-',
+  abilities: '-',
+  xp: '-',
+  imgUrl: '-'
 }
-// Main Function
-function insertPokemon() {
-  // Clear array list
-  resetPokemon()
 
-  // Data from Input
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+// Main Function
+function insertPokemon(pokepoke) {
   window
-    .fetch(`${pokeUrl}${inputName.value.toLowerCase()}`)
+    .fetch(`${pokeUrl}${pokepoke.toLowerCase()}`)
     .then((response) => {
       if ((response.status === 404) | (response.statusText == 'Not Found')) {
         alert('Pokemon not found. Try with another!')
@@ -53,81 +35,34 @@ function insertPokemon() {
       }
     })
     .then((responseJSON) => {
-      const allItems = []
-      const result = []
 
-      // JSON to Array
-      for (let pokemonInfo in responseJSON) {
-        result.push([pokemonInfo, responseJSON[pokemonInfo]])
-      }
-      console.table(result)
+      console.log(responseJSON)
+      pokemon.name = responseJSON.name
+      pokemon.height = responseJSON.height
+      pokemon.weight = responseJSON.weight
+      pokemon.xp = responseJSON.base_experience
+      pokemon.imgUrl = responseJSON.sprites.other.dream_world.front_default
 
-      // Pokemon Name
-      pokemonName.innerText =
-        `${result[10][1]} `.charAt(0).toUpperCase() +
-        `${result[10][1]} `.slice(1) +
-        ` N.°${result[6][1]}`
 
-      // Pokemon Image
-      pokemonImage.src = result[14][1].other.dream_world.front_default
-
-      // Type Title
-      pokemonTypeTitle = 'Type: '
-
-      // Pokemon Type-1
-      pokemonType1 = ` ${result[16][1][0].type.name}`
-
-      // Pokemon Type-2
-
-      if (result[16][1].length > 1) {
-        pokemonType2 = ` ${result[16][1][1].type.name}`
-      }
-
-      pokemonTypes.innerText = pokemonTypeTitle + pokemonType1 + pokemonType2
-
-      // Ability Title
-      pokemonAbilitiesTitle = 'Abilities: '
-
-      // Pokemon Ability-1
-      pokemonAbility1 = `${result[0][1][0].ability.name}`
-
-      // Pokemon Ability-2
-      if (result[0][1].length > 1) {
-        pokemonAbility2 = `, ${result[0][1][1].ability.name}`
-      }
-
-      // Pokemon Ability-3
-      if (result[0][1].length > 2) {
-        pokemonAbility3 = `, ${result[0][1][2].ability.name}`
-      }
-
-      pokemonAbilities.innerText =
-        pokemonAbilitiesTitle + pokemonAbility1 + pokemonAbility2 + pokemonAbility3
-
-      // Pokemon Experience
-      pokemonExperience.innerText = `Base Experience: ${result[1][1]}`
-
-      // Pokemon Height
-      pokemonHeight.innerText = 'Height: ' + `${result[4][1]}` * 10 + ' cm.'
-
-      // Pokemon Weight
-      pokemonWeight.innerText = 'Weight: ' + `${result[17][1]}` / 10 + ' kg.'
-
-      // Data Container
-      container.append(
-        pokemonImage,
-        pokemonName,
-        pokemonTypes,
-        pokemonAbilities,
-        pokemonExperience,
-        pokemonHeight,
-        pokemonWeight,
-      )
-
-      // Pushing Data from Container to allItems
-      allItems.push(container)
-      appNode.append(...allItems)
-
-      console.log(pokemonAbilitiesTitle, pokemonAbility1, pokemonAbility2, pokemonAbility3)
+      let types = []
+      responseJSON.types.forEach(x => {
+        types = [...types, capitalizeFirstLetter(x.type.name)]
+      })
+      pokemon.type = types.join(' - ')
+      
+      
+      let abilities = []
+      responseJSON.abilities.forEach(x => {
+        abilities = [...abilities, capitalizeFirstLetter(x.ability.name)]
+      })
+      pokemon.abilities = abilities.join(' - ')
+      
+      pokemonTypes.innerText = pokemon.type
+      pokemonWeight.innerText = pokemon.weight
+      pokemonHeight.innerText = pokemon.height
+      pokemonExperience.innerText = pokemon.xp
+      pokemonAbilities.innerText = pokemon.abilities
+      pokemonName.innerText = pokemon.name
+      pokemonImage.src = pokemon.imgUrl
     })
 }
